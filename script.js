1d54533565fe643621b93f6ab495efc9d20f8df6
Bhavi <129272547+Bhavi595@users.noreply.github.com>
2023-09-17 20:14:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let answer ='null'
+       for(let i =0; i<str.length; i++){
+           
+           if(str.charAt(i)==str.charAt(i+1)){
+                answer = str.charAt(i);
+                return answer;
+           }
+
+       }
+
+       return answer;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
