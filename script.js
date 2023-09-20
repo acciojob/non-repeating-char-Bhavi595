6cd26e17e02a646a34b62c766005b7c8ccb05282
Bhavi <129272547+Bhavi595@users.noreply.github.com>
2023-09-20 22:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function firstNonRepeatedChar(str) {
   for (let i = 0; i < str.length; i++) {
     if (obj[str[i]] !== undefined) {
         
-        obj[str[i]] =  obj[str(i)]+1;
+        obj[str[i]]  += 1;
     }
      else {
      
@@ -23,6 +23,8 @@ function firstNonRepeatedChar(str) {
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
 
 
 
